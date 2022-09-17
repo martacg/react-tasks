4d1d55fb7016bbf4113b23874b3e28a09505eb00
Marta González <martacg7@gmail.com>
2022-09-17 11:18:32 +0200

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'mi priemr tarea',
+        description: 'más info de mi primer tarea'
+    },
+    {
+        id: 1,
+        title: 'mi segunda tarea',
+        description: 'más info de mi segunda tarea'
+    },
+    {
+        id: 2,
+        title: 'mi tercer tarea',
+        description: 'más info de mi tercer tarea'
+    }
+]
